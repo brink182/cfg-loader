65e50aebf92b0ebc178232ce9b93a96d96d1d8dd
softwiiv2 <softwiiv2@googlemail.com>
2012-02-23 22:24:41 +0000

    -compiled with libogc svn r4884 (this should fix the newer wii motes) -fixed issue 9

--- source/version.h
+++ source/version.h
@@ -21,14 +21,14 @@
 #elif defined(BUILD_DBG) && (BUILD_DBG > 0)
 
    #define CFG_VERSION_STR VERSION_STR"-dbg" 
-   #define DEFAULT_IOS_IDX CFG_IOS_249 
+   #define DEFAULT_IOS_IDX CFG_IOS_AUTO 
    #define CFG_DEFAULT_PARTITION "auto" 
    #define CFG_HIDE_HDDINFO 1 
 
 #else
 
    #define CFG_VERSION_STR VERSION_STR"\0\0\0\0"
-   #define DEFAULT_IOS_IDX CFG_IOS_249 
+   #define DEFAULT_IOS_IDX CFG_IOS_AUTO 
    //#define CFG_DEFAULT_PARTITION "WBFS1" 
    #define CFG_DEFAULT_PARTITION "auto" 
    //#define CFG_HIDE_HDDINFO 0 

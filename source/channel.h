1aa2bcba928d424f0acff1aaafe746a4ed35b966
Howard_M_Busch <Howard_M_Busch@yahoo.com>
2013-03-31 04:25:07 +0000

    -Added Channel delete -Fixed Channel size display -Fixed Filters working within favorites

--- source/channel.h
+++ source/channel.h
@@ -6,3 +6,4 @@ char *get_name(u64 titleid);
 int CHANNEL_Banner(struct discHdr *hdr, SoundInfo *snd);
 u64 getChannelSize(struct discHdr *hdr);
 u64 getChannelReqIos(struct discHdr *hdr);
+s32 Channel_RemoveGame(struct discHdr *hdr);

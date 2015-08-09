615459b676afe633e5b3458aa2eccc94dfee2db2
brink182 <brink182@mailbox.org>
2015-08-09 23:02:39 +0200

    restructured base

---
+++ lib/libext2fs/source/wii_release/tdb.d
@@ -0,0 +1,4 @@
+tdb.o: c:/progging/cfg/Source/lib/libext2fs/source/tdb.c \
+ c:/progging/cfg/Source/lib/libext2fs/source/tdb.h
+
+c:/progging/cfg/Source/lib/libext2fs/source/tdb.h:
---
+++ lib/libntfs/src/source/wii_release/misc.d
@@ -0,0 +1,22 @@
+misc.o: c:/progging/cfg/Source/lib/libntfs/src/source/misc.c \
+ c:/progging/cfg/Source/lib/libntfs/src/source/config.h \
+ c:/progging/cfg/Source/lib/libntfs/src/source/types.h \
+ c:/devkitPro/libogc/include/gctypes.h \
+ c:/progging/cfg/Source/lib/libntfs/src/source/compat.h \
+ c:/progging/cfg/Source/lib/libntfs/src/source/mem_allocate.h \
+ c:/progging/cfg/Source/lib/libntfs/src/source/misc.h \
+ c:/progging/cfg/Source/lib/libntfs/src/source/logging.h
+
+c:/progging/cfg/Source/lib/libntfs/src/source/config.h:
+
+c:/progging/cfg/Source/lib/libntfs/src/source/types.h:
+
+c:/devkitPro/libogc/include/gctypes.h:
+
+c:/progging/cfg/Source/lib/libntfs/src/source/compat.h:
+
+c:/progging/cfg/Source/lib/libntfs/src/source/mem_allocate.h:
+
+c:/progging/cfg/Source/lib/libntfs/src/source/misc.h:
+
+c:/progging/cfg/Source/lib/libntfs/src/source/logging.h:
